8617065286e55bd27c8443510878a541e6437338
plakies <43638583+plakies@users.noreply.github.com>
2018-11-27 21:29:51 +0100

    He modificado 27/11
    He cambiado tilemaps y dos colliders del personaje para que actue mejor , tambn he hecho un scrip con links de musica para que mires si te gusta (en la carpeta musica)

---
+++ Assets/Music/MUSICA.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MUSICA : MonoBehaviour {
+
+    /*PASEO:https://www.youtube.com/watch?v=L5KeqtPf_nY&index=3&list=PLzSScg3-VWdzv4ZVkXXckPF5YZ-cTUSw0
+     *Casa:https://youtu.be/zytdQBD0Ya8?list=PLzSScg3-VWdzv4ZVkXXckPF5YZ-cTUSw0
+     * Salida Aventura:https://youtu.be/JgGV8oVGdeE?list=PLzSScg3-VWdzv4ZVkXXckPF5YZ-cTUSw0
+    */
+}
