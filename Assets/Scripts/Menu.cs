e2f6b29491e7ea480f871f4bf75cfe99486c6582
plakies <43638583+plakies@users.noreply.github.com>
2018-11-28 19:46:14 +0100

    Menu
    Falta sonido y solo se  ve bien si haces un maximize on play por alguna razon satanica que no entiendo

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Menu : MonoBehaviour
+{
+
+    // Use this for initialization
+    public void PlayGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("APLICACION CERRADA");
+        Application.Quit();
+    }
+
+}
