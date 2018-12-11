c5730fa0ff06deef927f5063b62a214b9cb64b9c
NightKillerGames <christianx1998@hotmail.com>
2018-12-11 19:01:58 +0100

    inventario a medias

---
+++ Assets/Scripts/InventoryController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryController : MonoBehaviour {
+
+    private static int numeroDeSlots = 2;
+    public bool[] isFull = new bool[numeroDeSlots];
+    public GameObject[] slots = new GameObject[numeroDeSlots];
+
+}
---
+++ Assets/Scripts/ItemsPickUp.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemsPickUp : MonoBehaviour {
+
+    private InventoryController inventory;
+    public GameObject objetoInventario;
+
+    void Start () {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<InventoryController>();
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            for (int i = 0; i < inventory.slots.Length; i++)
+            {
+                if (inventory.isFull[i] == false)
+                {
+
+                    inventory.isFull[i] = true;
+                    Instantiate(objetoInventario, inventory.slots[i].transform, false);
+                    Destroy(gameObject);
+                    break;
+                }
+                else
+                {
+
+                }
+            }
+        }
+    }
+}
